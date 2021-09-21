3fc1b978420ddc6fff92d3315e9182333fdc1c2b
KittyPiePie <dpro9173@gmail.com>
2021-09-21 02:06:05 -0500

    updated schema / imported tables from data

--- schema.sql
+++ schema.sql
@@ -1,17 +1,42 @@
 ﻿--- Create tables
 
 
-CREATE TABLE owid-covid-data_filtered
---
-	location  VARCHAR(40) NOT NULL,
-	total_cases int               ,        
-	total_deaths     int           ,            
+
+CREATE TABLE vax_manuf_filtered(
+	
+	location VARCHAR(40) NOT NULL,
+	Abdala                  int,
+	COVIran_Barekat         int,
+	CanSino                 int,
+	Covaxin                 int,
+	EpiVacCorona            int,
+	Johnson_Johnson         int,
+	Medigen                 int,
+	Moderna                 int,
+	Oxford_AstraZeneca      int,
+	Pfizer_BioNTech         int,
+	QazVac                  int,
+	Sinopharm_Beijing       int,
+	Sinopharm_HayatVax      int,
+	Sinopharm_Wuhan         int,
+	Sinovac                 int,
+	Soberana02              int,
+	Sputnik_V               int,
+	ZF2001                  int,
+	PRIMARY KEY(location)
+
+
+CREATE TABLE owid_covid_data_filtered(
+
+	location VARCHAR(40) NOT NULL,
+	total_cases float               ,        
+	total_deaths     float           ,            
 	total_cases_per_million   float ,               
 	total_deaths_per_million  float  ,            
 	reproduction_rate         float   ,             
-	total_vaccinations             int ,         
-	people_vaccinated                   int ,    
-	people_fully_vaccinated                int  ,
+	total_vaccinations             float ,         
+	people_vaccinated                   float ,    
+	people_fully_vaccinated                float  ,
 	total_vaccinations_per_hundred         float ,  
 	people_vaccinated_per_hundred          float  , 
 	people_fully_vaccinated_per_hundred    float   ,
@@ -30,37 +55,5 @@ CREATE TABLE owid-covid-data_filtered
 	hospital_beds_per_thousand              float   ,
 	life_expectancy                        float   ,
 	human_development_index float   ,
-	PRIMARY KEY (location),
-	FOREIGN KEY (location) REFERENCES vax_manuf_filtered(location)
+	PRIMARY KEY (location)
 );
-
-
-
-CREATE TABLE vax_manuf_filtered(
-	
-	location  PK  varchar(40)
-	Abdala                  int,
-	COVIran_Barekat         int,
-	CanSino                 int,
-	Covaxin                 int,
-	EpiVacCorona            int,
-	Johnson&Johnson         int,
-	Medigen                 int,
-	Moderna                 int,
-	Oxford/AstraZeneca      int,
-	Pfizer/BioNTech         int,
-	QazVac                  int,
-	Sinopharm/Beijing       int,
-	Sinopharm/HayatVax      int,
-	Sinopharm/Wuhan         int,
-	Sinovac                 int,
-	Soberana02              int,
-	Sputnik_V               int,
-	ZF2001                  int,
-	PRIMARY KEY(location),
-	
-	
-			
-
-
-
